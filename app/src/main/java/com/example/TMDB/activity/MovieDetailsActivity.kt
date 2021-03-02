d727cca8e711de1c76ad2afbf1bdfa1895ea556f
shamiul94 <1505038.sh@ugrad.cse.buet.ac.bd>
2021-03-03 00:01:34 +0600

    rating to decimal

--- app/src/main/java/com/example/TMDB/activity/MovieDetailsActivity.kt
+++ app/src/main/java/com/example/TMDB/activity/MovieDetailsActivity.kt
@@ -3,9 +3,12 @@ package com.example.TMDB.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.ImageView
 import android.widget.RatingBar
 import android.widget.TextView
 import com.example.TMDB.R
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.item_news.view.*
 
 class MovieDetailsActivity : AppCompatActivity() {
     private lateinit var movie_title: String
@@ -13,6 +16,7 @@ class MovieDetailsActivity : AppCompatActivity() {
     private lateinit var poster_path: String
     private lateinit var details: String
     private lateinit var vote_average: String
+    private lateinit var release_date: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,6 +27,7 @@ class MovieDetailsActivity : AppCompatActivity() {
         poster_path = intent.getStringExtra("poster_path").toString()
         details = intent.getStringExtra("details").toString()
         vote_average = intent.getStringExtra("vote_average").toString()
+        release_date = intent.getStringExtra("release_date").toString()
 
         Log.d("vote_average", vote_average)
         Log.d("movie_title", movie_title)
@@ -33,11 +38,22 @@ class MovieDetailsActivity : AppCompatActivity() {
         val overview_field: TextView = findViewById<TextView>(R.id.details_overview)
         val title_field: TextView = findViewById<TextView>(R.id.details_movie_title)
         val rating_star: RatingBar = findViewById(R.id.rating_bar)
-        val rating_text : TextView = findViewById(R.id.rating_text)
+        val rating_text: TextView = findViewById(R.id.rating_text)
+        val poster_image: ImageView = findViewById(R.id.details_poster)
+        val cover_image: ImageView = findViewById(R.id.details_cover)
+        val release_date_text: TextView = findViewById(R.id.release_date)
 
+        val a: Double = vote_average.toFloat()/2.0
         overview_field.text = details
         title_field.text = movie_title
-        rating_star.setRating(vote_average.toFloat())
+        rating_star.rating = a.toFloat()
         rating_text.text = vote_average
+        release_date_text.text = release_date
+
+        Picasso.get().load("https://image.tmdb.org/t/p/original" + poster_path)
+            .into(poster_image)
+
+        Picasso.get().load("https://image.tmdb.org/t/p/original" + back_drop_path)
+            .into(cover_image)
     }
 }
